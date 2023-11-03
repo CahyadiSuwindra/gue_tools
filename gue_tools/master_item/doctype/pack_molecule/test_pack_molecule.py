0cc3aa07d2d91c723284cfa6d68d9eccb2c97906
CahyadiSuwindra <149154168+CahyadiSuwindra@users.noreply.github.com>
2023-11-03 09:27:32 +0700

    Add master item
    Add master item module

---
+++ gue_tools/master_item/doctype/pack_molecule/test_pack_molecule.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, GUE Eco. and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPackMolecule(FrappeTestCase):
+	pass
